e87c8f0b69471550dedf4bf033a25acbf7a18792
Keith A. Schmidt <keithschmidt@live.com>
2019-04-08 22:03:52 -0500

    find_meteor.py uses requests and data from NASA to find meteor landing sites

--- find_meteors.py
+++ find_meteors.py
@@ -1,11 +1,4 @@
 # find-close-meteorites
-A demo project that uses Python and NASA data to find meteor landing sites
-
-## Running
-
-This project requires Python 3 and the requests package.
-
-'python3 find_meteors.py'
 
 import math
 import requests
